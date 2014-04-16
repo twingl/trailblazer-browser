fb9910368420be081fbe915e323ccfe08b3aea40
Greg Signal <greg@awesome.org.nz>
2014-04-16 16:14:56 +1200

    Remove lingering old code

--- APP_ROOT/templates/app.hbs.js
+++ APP_ROOT/templates/app.hbs.js
@@ -58,16 +58,3 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   return buffer;
   
 });
-Ember.TEMPLATES["test/world"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1;
-
-
-  data.buffer.push("<h1>Hello ");
-  stack1 = helpers._triageMustache.call(depth0, "view.message", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
-  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("</h1>\n<p>test!</p>\n");
-  return buffer;
-  
-});
