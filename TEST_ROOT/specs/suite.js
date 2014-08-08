2e6692bdf3b0661c1118afc0e0a2eea08be416be
Greg Signal <greg@awesome.org.nz>
2014-08-08 12:29:31 +1200

    Update SimpleAuth & include test helpers
    Also tag filenames with the version number

--- TEST_ROOT/specs/suite.js
+++ TEST_ROOT/specs/suite.js
@@ -4,3 +4,27 @@
   });
 
 }).call(this);
+
+(function() {
+  module("Integration: Login", {
+    setup: function() {
+      return chrome.identity.launchWebAuthFlow = function(args, cb) {
+        return console.log(args, cb);
+      };
+    },
+    teardown: function() {
+      return Twingl.reset();
+    }
+  });
+
+  test('do stuff', function() {
+    visit('login');
+    click('#tb-login button');
+    authenticateSession();
+    return andThen(function() {
+      console.log("test");
+      return equal(currentRouteName(), 'assignments');
+    });
+  });
+
+}).call(this);
